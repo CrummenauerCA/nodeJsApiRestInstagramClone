bbc1c17201903d09c11a0b0729f06f0ed1663680
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2019-07-19 09:59:12 -0300

    Starting to use MongoDB with mongoose...

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+mongoose.connect('mongodb://localhost:27017/backend', {
+  useNewUrlParser: true
+}).catch(error => {
+  console.log('ERRO: ', error)
+});
 
 app.get('/', (req, res) => {
   return res.send(`Olá ${req.query.name}`)
