780f167c40ebb73eeaec98068beaf65947f3065b
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2019-07-19 16:48:50 -0300

    Changing the extension name of images...

--- src/controller/PostController.js
+++ src/controller/PostController.js
@@ -13,12 +13,15 @@ module.exports = {
     const { author, place, description, hashtags } = req.body
     const { filename: image } = req.file
 
+    const [name] = image.split('.')
+    const fileName = `${name}.jpg`
+
     await sharp(req.file.path) // Compress the uploaded image
     .resize(480)
     .jpeg({
       quality: 50
     }).toFile(
-      path.resolve(req.file.destination, 'resized', image)
+      path.resolve(req.file.destination, 'resized', fileName)
     )
 
     fs.unlinkSync(req.file.path) // removes the original image
@@ -28,7 +31,7 @@ module.exports = {
       place,
       description,
       hashtags,
-      image
+      image: fileName
     })
     return res.json(post)
   }
