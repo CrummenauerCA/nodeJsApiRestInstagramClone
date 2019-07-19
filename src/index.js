e68bcd5b82edbed9d2150f666ffbdf152d98fe79
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2019-07-19 10:18:41 -0300

    Starting to use routes...

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express()
+
 mongoose.connect('mongodb://localhost:27017/backend', {
   useNewUrlParser: true
 }).catch(error => {
   console.log('ERRO: ', error)
 });
 
-app.get('/', (req, res) => {
-  return res.send(`Olá ${req.query.name}`)
-})
+app.use(require('./routes'))
 
 app.listen(3333)
